8792c10f9c72a778a191b284dec530cc813fdfcf
Youngmin-Yun <lauas15@naver.com>
2023-06-14 00:36:05 +0900

    Initial Commit

---
+++ yogiyo/yogiyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  yogiyoApp.swift
+//  yogiyo
+//
+//  Created by 윤영민 on 2023/06/14.
+//
+
+import SwiftUI
+
+@main
+struct yogiyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
